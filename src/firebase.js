bf6e06b76e2e635f5f7950c48978857b3d9e19bf
SeunghoLee <jimmylee871124@gmail.com>
2021-07-18 21:57:08 +0900

    [Initialization]React + Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDWwsLq85IFuYUP6IrGUXxxeMCp2BChy0",
+  authDomain: "twitter-clone-coding-621e3.firebaseapp.com",
+  projectId: "twitter-clone-coding-621e3",
+  storageBucket: "twitter-clone-coding-621e3.appspot.com",
+  messagingSenderId: "324320524848",
+  appId: "1:324320524848:web:fdebbfa890a18407db8bf5",
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
